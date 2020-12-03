794e13b88f1f08c06cc2de0753261401221d4fcc
Samantha Wittke <samantha.iwtte@nls.fi>
2020-12-03 18:37:29 +0200

    d3 done

--- day3.py
+++ day3.py
@@ -8,16 +8,14 @@ import math
 
 ##Part 1
 
-#3 right 1 down
+
 # load data
 def input_to_list(inputtxt):
     with open(inputtxt,'r') as inp:
         input = inp.readlines()
     #input has one string per line with /n in the end
-    print(input)
     input_wo_newline = [line.strip() for line in input] 
     # one string per row
-    print(input_wo_newline)
     return input_wo_newline
 
 def move_to_next(position, map, to_right, down):
@@ -28,22 +26,17 @@ def move_to_next(position, map, to_right, down):
     return position
 
 
-def count_trees(map):
+def count_trees(map,movement):
     position = (0,0)
     treecount = 0
-
     #as long as there is rows in the map..
     for line in range(0,len(map)):
-        
         #update position
-        position = move_to_next(position,map,3,1)
-
+        position = move_to_next(position,map,movement[0],movement[1])
         if position[0] <= len(map)-1:
-
             # adding line n amounts of times depending on how often needed
             if position[1]/len(map[position[0]]) >= 1:
                 map[position[0]] = (1 + int(position[1]/len(map[position[0]]))) * map[position[0]]
-
             # if the end poistion of one move is a 'tree', count it
             if map[position[0]][position[1]] == '#': 
                 treecount = treecount + 1
@@ -51,7 +44,6 @@ def count_trees(map):
     return treecount
     
 
-##Part 2
 
 
 
@@ -59,4 +51,15 @@ if __name__ =="__main__":
     inputtxt = sys.argv[1]
 
     onemap = input_to_list(inputtxt)
-    print(count_trees(onemap))
+
+    ##Part 1
+    print(count_trees(onemap, (3,1)))
+
+    ##Part 2
+
+    paths = [(1,1),(3,1),(5,1),(7,1),(1,2)]
+    product = 1
+    for path in paths:
+        
+        product = product *count_trees(onemap,path)
+    print(product)
