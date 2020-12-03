0905062f6ec52b22edc09cd61a04083c0b0a17a3
Samantha Wittke <samantha.wittke@nls.fi>
2020-12-03 15:54:48 +0200

    starting d3

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # adventofcode2020
+
+My try on advent of code 2020 with Python
+Comments and discussions very welcome :)
---
+++ day3.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+##Part 1
+
+inputtxt = 'd3_test.txt'
+#3 right 1 down
+
+# load data
+input = np.loadtxt(inputtxt)
+
+
+##Part 2
