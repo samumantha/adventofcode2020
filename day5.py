317e89090aea8e3003770b3f135ba2db3956de20
Samantha Wittke <samantha.iwtte@nls.fi>
2020-12-06 23:35:10 +0200

    d5 done

--- day5.py
+++ day5.py
@@ -1,4 +1,44 @@
 import sys
+"""
+def find_biggest(index):
+    biggestf = 0
+    biggestl = 0
+    for i,line in enumerate(input):
+        firstf = line.find('F') #the bigger the index, the higher the number
+        firstl = line.find('L') #same here
+        if firstf >= biggestf and firstl >= biggestl:
+            biggestf = firstf
+            biggestl = firstl
+            indexofbiggest = i 
+
+    print(input[indexofbiggest])
+"""
+def binary_check(input):
+    # F or L gives 0
+    highestid = 0
+    seatlist = []
+    for line in input:
+        line = line.replace('F','0')
+        line = line.replace('B','1')
+        line = line.replace('R','1')
+        line = line.replace('L','0')
+        seatid = int(line,2)
+        seatlist.append(seatid)
+        if seatid >= highestid:
+            highestid = seatid
+    return highestid, seatlist
+
+def find_my_seat(seatlist):
+    # max 1023
+    seatlist.sort()
+    fulllist = list(range(0,1023))
+    mismatches = [x for x in fulllist if not x in seatlist]
+    myseat = 0
+    for mismatch in mismatches:
+        if mismatch-1 in seatlist and mismatch+1 in seatlist:
+            #print(mismatch)
+            myseat = mismatch
+    return myseat
 
 
 if __name__ == "__main__":
@@ -8,15 +48,10 @@ if __name__ == "__main__":
         input = inp.readlines()
     input_wo_newline = [line.strip() for line in input] 
 
-    #rows 0 to 127
-    for line in input_wo_newline:
-        total = 128
-        for i,letter in enumerate(line):
-            if i < 7:
-                #F keeps lower
-                total = total / 2
-                if letter == 'F':
-                    up = total
-                    
-            else:
+#Part1
+    seatid,seatlist = binary_check(input_wo_newline)
+    print(seatid)
+
+#Part2
+    print(find_my_seat(seatlist))
 
